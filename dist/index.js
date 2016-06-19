a2cd42749ecfa765e2d7248c96d00171a614dd3d
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-06-19 10:04:24 +0300

    Add dist/ as this is a module

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.PGModel = undefined;
+
+var _PGModel = require('./pg/PGModel');
+
+var _PGModel2 = _interopRequireDefault(_PGModel);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.PGModel = _PGModel2.default;
